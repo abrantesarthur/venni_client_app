fe19a2eb5f95099e5d09953f3a37d49382f84816
arthur_abrantes <arthur@venni.app>
2021-07-21 22:31:19 -0300

    update url types

--- lib/screens/insertPhone.dart
+++ lib/screens/insertPhone.dart
@@ -125,6 +125,7 @@ class InsertPhoneNumberState extends State<InsertPhone> {
     FirebaseAuth firebaseAuth,
     FirebaseDatabase firebaseDatabase,
   ) async {
+    print("buttonCallback");
     if (phoneNumber != null) {
       // prevent users from editing phone number and show loading icon
       setState(() {
@@ -144,12 +145,15 @@ class InsertPhoneNumberState extends State<InsertPhone> {
                 firebaseDatabase: firebaseDatabase,
                 firebaseAuth: firebaseAuth,
                 onExceptionCallback: (FirebaseAuthException e) {
+                  print(e);
                   setInactiveState(
                     message: "Algo deu errado. Tente novamente.",
                   );
                 });
           },
           verificationFailed: (FirebaseAuthException e) {
+            print("verificationFailed");
+            print(e);
             String errorMsg = firebaseAuth.verificationFailedCallback(e);
             setInactiveState(message: errorMsg);
           },
